66320dd23030a6cc8ecbcb36750bf098d4dacfb7
prinata.rakha <prinata.rakha@ui.ac.id>
2021-10-24 19:45:32 +0700

    buat model dan form baru, html untuk form baru juga

--- vaksin/models.py
+++ vaksin/models.py
@@ -1,10 +1,38 @@
 from django.db import models
 
 # Create your models here.
-class Vaksin(models.Model):
-    nama            = models.CharField(max_length=100)
-    url             = models.CharField(max_length=100)
-    alamat          = models.CharField(max_length=100)
-    nomorTelp       = models.IntegerField() # max 12 digit
-    jenis           = models.CharField(max_length=100)
-    syaratPeserta   = models.CharField(max_length=100)
+class VaksinJakarta(models.Model):
+    nama = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=100)
+    nomorTelp = models.IntegerField() # max 12 digit
+    jenis = models.CharField(max_length=100)
+    syaratPeserta = models.CharField(max_length=100)
+class VaksinBogor(models.Model):
+    nama = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=100)
+    nomorTelp = models.IntegerField() # max 12 digit
+    jenis = models.CharField(max_length=100)
+    syaratPeserta = models.CharField(max_length=100)
+class VaksinDepok(models.Model):
+    nama = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=100)
+    nomorTelp = models.IntegerField() # max 12 digit
+    jenis = models.CharField(max_length=100)
+    syaratPeserta = models.CharField(max_length=100)
+class VaksinTangerang(models.Model):
+    nama = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=100)
+    nomorTelp = models.IntegerField() # max 12 digit
+    jenis = models.CharField(max_length=100)
+    syaratPeserta = models.CharField(max_length=100)
+class VaksinBekasi(models.Model):
+    nama = models.CharField(max_length=100)
+    url = models.CharField(max_length=100)
+    alamat = models.CharField(max_length=100)
+    nomorTelp = models.IntegerField() # max 12 digit
+    jenis = models.CharField(max_length=100)
+    syaratPeserta = models.CharField(max_length=100)
--- vaksin/views.py
+++ vaksin/views.py
@@ -1,21 +1,80 @@
-from django.shortcuts import render
-from .models import Vaksin
-from .forms import VaksinForm
+from django.shortcuts import render, redirect
+from .models import VaksinJakarta, VaksinBogor, VaksinDepok, VaksinTangerang, VaksinBekasi
+from .forms import FormJakarta, FormBogor, FormDepok, FormTangerang, FormBekasi
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-    vaksins = Vaksin.objects.all().values()
-    response = {'vaksin': vaksins}
+    vaksinsJak = VaksinJakarta.objects.all().values()
+    vaksinsBog = VaksinBogor.objects.all().values()
+    vaksinsDep = VaksinDepok.objects.all().values()
+    vaksinsTag = VaksinTangerang.objects.all().values()
+    vaksinsBek = VaksinBekasi.objects.all().values()
+    response = {
+        'vaksinsJak': vaksinsJak,
+        'vaksinsBog': vaksinsBog,
+        'vaksinsDep': vaksinsDep,
+        'vaksinsTag': vaksinsTag,
+        'vaksinsBek': vaksinsBek,
+    }
     return render(request, 'index_vaksin.html', response)
 
-def add_vaksin(request):
-    form = VaksinForm()
+@login_required(login_url='/admin/login/')
+def add_jakarta(request):
+    form = FormJakarta()
 
     if request.method == "POST":
-        form = VaksinForm(request.POST)
+        form = FormJakarta(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect(index)
+            return redirect(index)
     context = {'form': form}
-    return render(request, "form_vaksin.html", context)
+    return render(request, "form_jakarta.html", context)
 
+@login_required(login_url='/admin/login/')
+def add_bogor(request):
+    form = FormBogor()
+
+    if request.method == "POST":
+        form = FormBogor(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+    context = {'form': form}
+    return render(request, "form_bogor.html", context)
+
+@login_required(login_url='/admin/login/')
+def add_depok(request):
+    form = FormDepok()
+
+    if request.method == "POST":
+        form = FormDepok(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+    context = {'form': form}
+    return render(request, "form_depok.html", context)
+
+@login_required(login_url='/admin/login/')
+def add_tangerang(request):
+    form = FormTangerang()
+
+    if request.method == "POST":
+        form = FormTangerang(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+    context = {'form': form}
+    return render(request, "form_tangerang.html", context)
+
+@login_required(login_url='/admin/login/')
+def add_bekasi(request):
+    form = FormBekasi()
+
+    if request.method == "POST":
+        form = FormBekasi(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(index)
+    context = {'form': form}
+    return render(request, "form_bekasi.html", context)
