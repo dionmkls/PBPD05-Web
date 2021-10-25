2409e5c855c777c5b565808ddea71aa751aced9c
prinata.rakha <prinata.rakha@ui.ac.id>
2021-10-25 16:31:28 +0700

    finalisasi css untuk page form dan index

--- vaksin/models.py
+++ vaksin/models.py
@@ -2,37 +2,37 @@ from django.db import models
 
 # Create your models here.
 class VaksinJakarta(models.Model):
-    nama = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    alamat = models.CharField(max_length=100)
+    nama = models.CharField(max_length=50)
+    url = models.CharField(max_length=1000)
+    alamat = models.CharField(max_length=50)
     nomorTelp = models.IntegerField() # max 12 digit
-    jenis = models.CharField(max_length=100)
-    syaratPeserta = models.CharField(max_length=100)
+    jenis = models.CharField(max_length=50)
+    syaratPeserta = models.CharField(max_length=50)
 class VaksinBogor(models.Model):
-    nama = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    alamat = models.CharField(max_length=100)
+    nama = models.CharField(max_length=50)
+    url = models.CharField(max_length=1000)
+    alamat = models.CharField(max_length=50)
     nomorTelp = models.IntegerField() # max 12 digit
-    jenis = models.CharField(max_length=100)
-    syaratPeserta = models.CharField(max_length=100)
+    jenis = models.CharField(max_length=50)
+    syaratPeserta = models.CharField(max_length=50)
 class VaksinDepok(models.Model):
-    nama = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    alamat = models.CharField(max_length=100)
+    nama = models.CharField(max_length=50)
+    url = models.CharField(max_length=1000)
+    alamat = models.CharField(max_length=50)
     nomorTelp = models.IntegerField() # max 12 digit
-    jenis = models.CharField(max_length=100)
-    syaratPeserta = models.CharField(max_length=100)
+    jenis = models.CharField(max_length=50)
+    syaratPeserta = models.CharField(max_length=50)
 class VaksinTangerang(models.Model):
-    nama = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    alamat = models.CharField(max_length=100)
+    nama = models.CharField(max_length=50)
+    url = models.CharField(max_length=1000)
+    alamat = models.CharField(max_length=50)
     nomorTelp = models.IntegerField() # max 12 digit
-    jenis = models.CharField(max_length=100)
-    syaratPeserta = models.CharField(max_length=100)
+    jenis = models.CharField(max_length=50)
+    syaratPeserta = models.CharField(max_length=50)
 class VaksinBekasi(models.Model):
-    nama = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    alamat = models.CharField(max_length=100)
+    nama = models.CharField(max_length=50)
+    url = models.CharField(max_length=1000)
+    alamat = models.CharField(max_length=50)
     nomorTelp = models.IntegerField() # max 12 digit
-    jenis = models.CharField(max_length=100)
-    syaratPeserta = models.CharField(max_length=100)
+    jenis = models.CharField(max_length=50)
+    syaratPeserta = models.CharField(max_length=50)
