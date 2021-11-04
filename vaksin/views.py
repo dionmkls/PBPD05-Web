56aa850a38d50e29d5e77a813905388abfe2c72b
prinata.rakha <prinata.rakha@ui.ac.id>
2021-11-05 00:13:56 +0700

    insya Allah udah kelar semua

--- vaksin/views.py
+++ vaksin/views.py
@@ -1,75 +1,85 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import VaksinJakarta, VaksinBogor, VaksinDepok, VaksinTangerang, VaksinBekasi
 from .forms import FormJakarta, FormBogor, FormDepok, FormTangerang, FormBekasi
 from django.contrib.auth.decorators import login_required
-from django.core import serializers
 from django.http import JsonResponse
 
 # Create your views here.
-def index(request):
-    vaksinsJak = VaksinJakarta.objects.all().values()
-    vaksinsBog = VaksinBogor.objects.all().values()
-    vaksinsDep = VaksinDepok.objects.all().values()
-    vaksinsTag = VaksinTangerang.objects.all().values()
-    vaksinsBek = VaksinBekasi.objects.all().values()
-    response = {
-        'vaksinsJak': vaksinsJak,
-        'vaksinsBog': vaksinsBog,
-        'vaksinsDep': vaksinsDep,
-        'vaksinsTag': vaksinsTag,
-        'vaksinsBek': vaksinsBek,
-    }
-    if request.user.is_authenticated:
-        return render(request, 'index_vaksin2.html', response)
-    else:
-        return render(request, 'index_vaksin.html', response)
-
-
-
-
-
-
-
-
-
 def index2(request): #nampilin data pake ajax
-    # vaksinsJak = serializers.serialize('json', VaksinJakarta.objects.all())
-    # vaksinsBog = serializers.serialize('json', VaksinBogor.objects.all())
-    # vaksinsDep = serializers.serialize('json', VaksinDepok.objects.all())
-    # vaksinsTag = serializers.serialize('json', VaksinTangerang.objects.all())
-    # vaksinsBek = serializers.serialize('json', VaksinBekasi.objects.all())
-    # vaksinsJak = VaksinJakarta.objects.all()
-    # vaksinsBog = VaksinBogor.objects.all()
-    # vaksinsDep = VaksinDepok.objects.all()
-    # vaksinsTag = VaksinTangerang.objects.all()
-    # vaksinsBek = VaksinBekasi.objects.all()
+    if request.is_ajax():
+        if request.GET['tujuan'] == "delete":
+            print(request)
+            return delete_vaksin(request)
+        else: # nampilin data
+            print('tampil data')
+            return JsonResponse(data_to_array(), safe=False)
 
-    # hapus data
+    if request.user.is_authenticated:
+        return render(request, 'index_vaksin2.html', {})
+    else:
+        return render(request, 'index_vaksin.html', {})
 
+def data_to_array():
     vaksinsJak = VaksinJakarta.objects.all().values()
     vaksinsBog = VaksinBogor.objects.all().values()
     vaksinsDep = VaksinDepok.objects.all().values()
     vaksinsTag = VaksinTangerang.objects.all().values()
     vaksinsBek = VaksinBekasi.objects.all().values()
 
-    # ubah queryset ke list python
+    arr_data = dict()
+    arr = []
+    for i in vaksinsJak:
+        arr += [i]
+    arr_data['Jak'] = arr
     
-    response = {
-        'vaksinsJak': vaksinsJak,
-        'vaksinsBog': vaksinsBog,
-        'vaksinsDep': vaksinsDep,
-        'vaksinsTag': vaksinsTag,
-        'vaksinsBek': vaksinsBek,
-    }
+    arr = []
+    for i in vaksinsBog:
+        arr += [i]
+    arr_data['Bog'] = arr
+    
+    arr = []
+    for i in vaksinsDep:
+        arr += [i]
+    arr_data['Dep'] = arr
+    
+    arr = []
+    for i in vaksinsTag:
+        arr += [i]
+    arr_data['Tag'] = arr
+    
+    arr = []
+    for i in vaksinsBek:
+        arr += [i]
+    arr_data['Bek'] = arr
 
-    # ngapus data, ditampilin ulang
-    return render(request, 'index_vaksin3.html', JsonResponse(response))
 
+    return arr_data
+    
 
+def delete_vaksin(request):
+     # ambil data yg mau diapus 
+    kota = request.GET['kota']
+    id = request.GET['id']
 
+    if kota == "Jakarta":
+        obj = get_object_or_404(VaksinJakarta, id = id)
+    elif kota == "Bogor":
+        obj = get_object_or_404(VaksinBogor, id = id)
+    elif kota == "Depok":
+        obj = get_object_or_404(VaksinDepok, id = id)
+    elif kota == "Tangerang":
+        obj = get_object_or_404(VaksinTangerang, id = id)
+    elif kota == "Bekasi":
+        obj = get_object_or_404(VaksinBekasi, id = id)
 
+    nama = obj.nama
+    print(nama + " berhasil di delete")
+    # delete object
+    obj.delete()
 
-
+    return JsonResponse({
+        'nama': nama
+    })
 
 
 
@@ -82,11 +92,19 @@ def index2(request): #nampilin data pake ajax
 def add_jakarta(request):
     form = FormJakarta()
 
-    if request.method == "POST":
+    if request.is_ajax():
         form = FormJakarta(request.POST)
         if form.is_valid():
+            print("valid")
             form.save()
-            return redirect(index)
+            return JsonResponse({
+                'message': 'success',
+                'nama': request.POST['nama'],
+            })
+        else:
+            print("ga valid")
+    else:
+        print("bukan ajax")
     context = {'form': form}
     return render(request, "form_jakarta.html", context)
 
@@ -94,11 +112,20 @@ def add_jakarta(request):
 def add_bogor(request):
     form = FormBogor()
 
-    if request.method == "POST":
+    if request.is_ajax():
         form = FormBogor(request.POST)
         if form.is_valid():
+            print("valid")
             form.save()
-            return redirect(index)
+            return JsonResponse({
+                'message': 'success',
+                'nama': request.POST['nama'],
+            })
+        else:
+            print("ga valid")
+    else:
+        print("bukan ajax")
+
     context = {'form': form}
     return render(request, "form_bogor.html", context)
 
@@ -106,11 +133,20 @@ def add_bogor(request):
 def add_depok(request):
     form = FormDepok()
 
-    if request.method == "POST":
+    if request.is_ajax():
         form = FormDepok(request.POST)
         if form.is_valid():
+            print("valid")
             form.save()
-            return redirect(index)
+            return JsonResponse({
+                'message': 'success',
+                'nama': request.POST['nama'],
+            })
+        else:
+            print("ga valid")
+    else:
+        print("bukan ajax")
+
     context = {'form': form}
     return render(request, "form_depok.html", context)
 
@@ -118,11 +154,20 @@ def add_depok(request):
 def add_tangerang(request):
     form = FormTangerang()
 
-    if request.method == "POST":
+    if request.is_ajax():
         form = FormTangerang(request.POST)
         if form.is_valid():
+            print("valid")
             form.save()
-            return redirect(index)
+            return JsonResponse({
+                'message': 'success',
+                'nama': request.POST['nama'],
+            })
+        else:
+            print("ga valid")
+    else:
+        print("bukan ajax")
+
     context = {'form': form}
     return render(request, "form_tangerang.html", context)
 
@@ -130,10 +175,19 @@ def add_tangerang(request):
 def add_bekasi(request):
     form = FormBekasi()
 
-    if request.method == "POST":
+    if request.is_ajax():
         form = FormBekasi(request.POST)
         if form.is_valid():
+            print("valid")
             form.save()
-            return redirect(index)
+            return JsonResponse({
+                'message': 'success',
+                'nama': request.POST['nama'],
+            })
+        else:
+            print("ga valid")
+    else:
+        print("bukan ajax")
+
     context = {'form': form}
     return render(request, "form_bekasi.html", context)
