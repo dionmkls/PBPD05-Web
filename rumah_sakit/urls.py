0d1afb65ea36a681195d9186e61b3b6e0f63d48a
Qosim Ariqoh Daffa <qosim.ariqoh@ui.ac.id>
2021-11-05 13:47:56 +0700

    Update Folder Rumah Sakit

--- rumah_sakit/urls.py
+++ rumah_sakit/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 
-from .views import index, add_rs
+from .views import index, add_rs, edit_rs, hapus_rs
 
 urlpatterns = [
     path('', index, name='rs_index'),
     path('add/', add_rs, name="rs_form"),
+    path('<id>/edit/', edit_rs),
+    path('<id>/hapus/', hapus_rs),
     path('admin/login', auth_views.LoginView.as_view()),
 ]
---
+++ static/qosim/js/index.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//     $('#addBtn').click();
+
+//     $('#editBtn').click(function(){
+//         var nama = document.getElementById('nama');
+//         alert("Rumah sakit " + nama + " berhasil diperbarui");
+//     });
+
+//     $('#removeBtn').click(function(){
+//         var nama = document.getElementById('nama');
+//         alert("Rumah sakit " + nama + " berhasil dihapus");
+//     });
+// });
+
+// function addForm(){
+//     alert("Tes");
+// }
