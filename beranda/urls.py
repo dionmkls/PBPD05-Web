dbc5486ebb5107085224b5705075ce88a9ce6cb7
Amelia Putri Fadillah <ameliaputrifadillah@gmail.com>
2021-11-05 12:39:03 +0700

    update beranda

--- beranda/urls.py
+++ beranda/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import index, get_covid_data, add_covid_data, update_covid_data, delete_covid_data
+from .views import index, Covid, get_covid_data, add_covid_data, update_covid_data, delete_covid_data
 
 
 urlpatterns = [
     path('', index, name='beranda'),
-    path('edit-covid-data/', add_covid_data ,name='edit-covid-data'),
+    path('edit-covid-data/', Covid ,name='edit-covid-data'),
     path('add-covid-data/', add_covid_data ,name='add-covid-data'),
     path('get-covid-data/<id>/', get_covid_data ,name='get-covid-data/data_id'),
-    path('update-covid-data/<id>/', update_covid_data ,name='update-covid-data/data_id'),
-    path('delete-covid-data/<id>/', delete_covid_data ,name='delete-covid-data/data_id'),   
+    path('edit-covid-data/<id>/update', update_covid_data ,name='update-covid-data/data_id'),
+    path('edit-covid-data/<id>/delete', delete_covid_data ,name='delete-covid-data/data_id'),   
 ]
