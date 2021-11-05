f1c948a7874e7ddc759fad6cdb8f97b6d28c44d4
dhekmass12 <dimas.tri01@ui.ac.id>
2021-11-05 23:15:30 +0700

    add ajax and json to oksigen

--- oksigen/urls.py
+++ oksigen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index, add_oksigen
+from .views import index, add_oksigen, json
 
 urlpatterns = [
     path('', index, name='index'),
-    path('add', add_oksigen, name='add_oksigen')
+    path('add', add_oksigen, name='add_oksigen'),
+    path('json', json, name='json')
 ]
