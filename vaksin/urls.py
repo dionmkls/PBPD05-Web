bd45d122b9280f91a9e053b783474c17451f2dfd
prinata.rakha <prinata.rakha@ui.ac.id>
2021-12-28 16:02:30 +0700

    buat webservice bagian vaksin

--- vaksin/urls.py
+++ vaksin/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import add_jakarta, add_bekasi, add_bogor, add_depok, add_tangerang, index2
+from .views import add_jakarta, add_bekasi, add_bogor, add_depok, add_tangerang, index2, add, index_flutter
 
 urlpatterns = [
     path('', index2, name='index2'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('add-vaksin-depok', add_depok, name='add_depok'),
     path('add-vaksin-tangerang', add_tangerang, name='add_tangerang'),
     path('add-vaksin-bekasi', add_bekasi, name='add_bekasi'),
+    path('add', add, name="add"),
+    path('flutter-web-service', index_flutter, name='index_flutter'),
 ]
