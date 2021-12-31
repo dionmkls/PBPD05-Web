52f5a13d387df3f1e57d8c01c1f047a1da0e2927
Rheina Elvaretta <rheina.elvaretta@ui.ac.id>
2021-12-31 13:32:37 +0700

    Add Webservice Requirements

--- forum/urls.py
+++ forum/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import index, add_forum, delete_forum
+from .views import index, add_forum, delete_forum, add_flutter, json_flutter
 
 urlpatterns = [
     path('', index, name='index'),
     path('add-forum/', add_forum, name='add_forum'),
-    path('delete-forum/<id>',delete_forum, name='delete_forum' )
+    path('delete-forum/<id>',delete_forum, name='delete_forum' ),
+    path('webservice-forum', add_flutter, name='add_flutter'),
+    path('json', json_flutter, name='json'),
 ]
